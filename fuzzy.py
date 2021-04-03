197283f624e7b65f3319fe741a386548aace2382
cathlx <77919590+cathlx@users.noreply.github.com>
2021-04-03 13:09:16 +0300

    Update fuzzy.py

--- fuzzy.py
+++ fuzzy.py
@@ -3,12 +3,14 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 standard = 'жадина-говядина, соленый огурец, на полу валяется, никто его не ест'
+d = {}
 
 with open('greedy.csv', newline = '', encoding = 'utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
-        item = row['text'].replace('.', '').lower()
-        print('Ratio of \'{0}\' and \'{1}\' is {2}.'.format(item, standard, fuzz.ratio(row['text'], standard)))
-        print('Partial ratio of \'{0}\' and \'{1}\' is {2}.'.format(item, standard, fuzz.partial_ratio(row['text'], standard)))
-
-
+        item = row['text'].strip().replace('.', '').lower()
+        fratio = int(fuzz.ratio(item, standard))
+        print('Ratio of \'{0}\' and \'{1}\' is {2}.'.format(item, standard, fratio))
+        d[fratio] = d.get(fratio, set()) | set([item])
+    for key, values in sorted(d.items(), key = lambda x: -x[0]):
+        print(key, values)
